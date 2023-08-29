ec164d865bc4977e3d5f90483c83ccc260846c61
Hasibmihad <mdhasibuddin12@gmail.com>
2023-08-29 22:43:26 +0600

    final final

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,3 +1,5 @@
+''' function : list divide'''
+
 def list_divide(numbers,divide=2):
     count=0
     for x in numbers:
@@ -9,20 +11,28 @@ def list_divide(numbers,divide=2):
 
 
 
+''' class : list divide exception'''
+
 class ListDivideException(Exception):
     pass
 
 
+''' function :test_list divide'''
+
 def test_list_divide():
     
     try:
-        result=list_divide([1,2,3,4,5])
+        ''' should result 2, if not ,raise exception '''
+        result=list_divide([1,2,3,4,5])   
         if result !=2:
             raise ListDivideException
     except ListDivideException:
         print("Exception occurred: Invalid Output")   
 
+
+
     try:
+        ''' should result 5, if not ,raise exception '''
         result=list_divide([2,4,6,8,10])
         if result !=5:
             raise ListDivideException
@@ -31,6 +41,7 @@ def test_list_divide():
 
 
     try:
+        ''' should result 2, if not ,raise exception '''
         result=list_divide([30, 54, 63,98, 100], divide=10) 
         if result !=2:
             raise ListDivideException
@@ -40,6 +51,7 @@ def test_list_divide():
 
 
     try:
+        ''' should result 0, if not ,raise exception '''
         result=list_divide([])
         if result !=0:
             raise ListDivideException
@@ -48,6 +60,7 @@ def test_list_divide():
 
 
     try:
+        ''' should result 1, if not ,raise exception '''
         result=list_divide([1,2,3,4,5], 1)
         if result !=5:
             raise ListDivideException
