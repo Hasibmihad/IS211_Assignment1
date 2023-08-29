af2b309f17d3d7a7fc106b60d54df7129539a99d
Hasibmihad <mdhasibuddin12@gmail.com>
2023-08-29 22:24:44 +0600

    main deploy

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -5,4 +5,30 @@ def list_divide(numbers,divide=2):
             count=count+1
         else :
             continue 
-    return count                  
+    return count    
+
+
+
+class ListDivideException(Exception):
+    pass
+
+
+def test_list_divide():
+        
+    try:
+        result=list_divide([1,2,3,4,5])
+        if result!=3:
+            raise ListDivideException("Test failed")
+    except ListDivideException:
+        print("Exception occurred: Invalid Output")    
+        
+def main():
+    try:
+        test_list_divide()
+    except ListDivideException as e:
+        print("Test failed: {e}")
+
+if __name__ == "__main__":
+    main()
+
+
