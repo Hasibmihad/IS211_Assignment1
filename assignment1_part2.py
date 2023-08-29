1bf8cb2987ebee89eedbee686d9c479f3f914daa
Hasibmihad <mdhasibuddin12@gmail.com>
2023-08-29 23:38:03 +0600

    none word fixing

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,12 +1,22 @@
 class Book:
-    author = ""
-    title = ""
+    #author = ""
+    #title = ""
     def __init__(self,author,title):
-        Book.author= author
-        Book.title= title 
+        self.author= author
+        self.title= title 
 
     def display(self):  
-        str= Book.author+", written by "+Book.author
+        str= self.title+", written by "+self.author
         print (str)
+        return 
 
-        
+def main():
+    book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+
+    # Print the objects using class Books display() function
+    print(book1.display())
+    print(book2.display())
+
+if __name__ == "__main__":
+    main()
