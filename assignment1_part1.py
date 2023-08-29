d4af5881573827396c84f783a9ec2d9c3263c327
Hasibmihad <mdhasibuddin12@gmail.com>
2023-08-29 22:39:16 +0600

    final

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -14,19 +14,52 @@ class ListDivideException(Exception):
 
 
 def test_list_divide():
-        
+    
     try:
         result=list_divide([1,2,3,4,5])
-        if result!=3:
-            raise ListDivideException("Test failed")
+        if result !=2:
+            raise ListDivideException
     except ListDivideException:
-        print("Exception occurred: Invalid Output")    
+        print("Exception occurred: Invalid Output")   
+
+    try:
+        result=list_divide([2,4,6,8,10])
+        if result !=5:
+            raise ListDivideException
+    except ListDivideException:
+        print("Exception occurred: Invalid Output")  
+
+
+    try:
+        result=list_divide([30, 54, 63,98, 100], divide=10) 
+        if result !=2:
+            raise ListDivideException
+    except ListDivideException:
+        print("Exception occurred: Invalid Output")   
+
+
+
+    try:
+        result=list_divide([])
+        if result !=0:
+            raise ListDivideException
+    except ListDivideException:
+        print("Exception occurred: Invalid Output")  
+
+
+    try:
+        result=list_divide([1,2,3,4,5], 1)
+        if result !=5:
+            raise ListDivideException
+    except ListDivideException:
+        print("Exception occurred: Invalid Output")              
+
         
 def main():
     try:
         test_list_divide()
-    except ListDivideException as e:
-        print("Test failed: {e}")
+    except ListDivideException:
+        print()
 
 if __name__ == "__main__":
     main()
