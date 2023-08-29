b3baadabfdd8c6700f510d44f219dfdfa0fc4ea9
Hasibmihad <mdhasibuddin12@gmail.com>
2023-08-29 23:39:05 +0600

    fixed

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -8,15 +8,14 @@ class Book:
     def display(self):  
         str= self.title+", written by "+self.author
         print (str)
-        return 
 
 def main():
     book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
     book2 = Book("Walter Scott", "Ivanhoe: A Romance")
 
     # Print the objects using class Books display() function
-    print(book1.display())
-    print(book2.display())
+    book1.display()
+    book2.display()
 
 if __name__ == "__main__":
     main()
